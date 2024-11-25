471d47b61746077b5aa27bba0376e4d4c55052f2
Apoorva Kashi <apoorvakashi@Apoorvas-MacBook-Air-2.local>
2024-11-24 19:59:26 -0500

    Search History Page and 404 Error Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,9 @@ import HomePage from "./pages/home/HomePage";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 import WatchPage from "./pages/WatchPage";
-import SearchPage from "./pages/searchPage";
+import SearchPage from "./pages/SearchPage";
+import NotFoundPage from "./pages/404";
+import SearchHistoryPage from "./pages/SearchHistoryPage";
 import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
 import { useAuthStore } from "./store/authUser";
@@ -37,6 +39,10 @@ function App() {
       <Route path="/login"element={user?  <Navigate to={"/"} /> :<LoginPage/>} />
       <Route path="/watch/:id"element={user? <WatchPage/> : <Navigate to={"/login"} /> } />
       <Route path="/search"element={user? <SearchPage/> : <Navigate to={"/login"} /> } />
+      <Route path="/history"element={user? <SearchHistoryPage/> : <Navigate to={"/login"} /> } />
+      <Route path="/*"element= {<NotFoundPage/>}/>
+
+
 
 
 
