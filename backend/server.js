56f27f66a23992a3de0db7cbd6b0abefb8f3afa4
Apoorva Kashi <apoorvakashi@Apoorvas-MacBook-Air-2.local>
2024-11-26 12:08:16 -0500

    Small fixes

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
 import searchRoutes from "./routes/search.route.js";
-
+import path from 'path';
 
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
@@ -14,6 +14,7 @@ import { protectRoutes } from './middleware/protectRoutes.js';
 
 const app = express();
 const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -22,8 +23,12 @@ app.use("/api/v1/movie", protectRoutes, movieRoutes)
 app.use("/api/v1/tv", protectRoutes, tvRoutes)
 app.use("/api/v1/search", protectRoutes, searchRoutes)
 
-
-
+if (ENV_VARS.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     console.log("Server started at http://localhost:" + PORT);
